e62ca2318bf5ed85382c49fd992d8eb66c4950ef
Ori Menachem Levi <orimenachemlevi@gmail.com>
2023-03-29 00:33:57 +0300

    - fix modal style

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -9,26 +9,30 @@ interface IModalProps {
 }
 
 function Modal({ closeModal, title, children }: IModalProps) {
-    return (
 
-        <>
-            <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed -top-28 inset-0 z-50 outline-none focus:outline-none">
-                <div className="relative w-full h-80 my-6 mx-auto max-w-3xl">
-                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col  bg-white outline-none focus:outline-none h-fit m-auto w-2/3">
-                        <div className="flex items-center justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-                            <button
-                                className="bg-transparent border-0 text-gray-900 [&>*]:text-gray-900"
-                                onClick={() => closeModal(false)}
-                            >
-                                <CloseIcon />
-                            </button>
-                            <h2 className="text-3xl font=semibold">{title}</h2>
-                        </div>
-                        {children}
+    function stopPropogation(e: React.MouseEvent<HTMLElement>) {
+        e.stopPropagation();
+        // closeModal(false);
+    };
+    const Children = () => <div onClick={stopPropogation}>{children}</div>
+
+    return (
+        <div onClick={() => closeModal(false)} className="flex justify-center items-center overflow-x-hidden overflow-y-auto bg-zinc-900/[.2] fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-full my-6 mx-auto max-w-3xl">
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col  bg-white outline-none focus:outline-none h-fit m-auto w-2/3">
+                    <div className="flex items-center justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                        <button
+                            className="bg-transparent border-0 text-gray-900 [&>*]:text-gray-900"
+                            onClick={() => closeModal(false)}
+                        >
+                            <CloseIcon />
+                        </button>
+                        <h2 className="text-3xl font=semibold">{title}</h2>
                     </div>
+                    <Children />
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
